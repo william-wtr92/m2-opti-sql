2aae2a9ed2ed587d2db3ff2790368460427e9692
william-wtr92 <wwautrin@hotmail.fr>
2024-11-12 11:36:23 +0100

    feat(exo1): swagger

--- exerice1/src/main/java/com/example/exerice1/config/OpenAPIConfig.java
+++ exerice1/src/main/java/com/example/exerice1/config/OpenAPIConfig.java
@@ -2,8 +2,6 @@ package com.example.exerice1.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
